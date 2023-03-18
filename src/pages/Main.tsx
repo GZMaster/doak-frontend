c569d64dee77754806179df12ad5515e88c605f5
Osianic <56081021+Greg-Soap@users.noreply.github.com>
2023-03-18 19:33:36 +0100

    navbar completed

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,8 +1,11 @@
 import Footer from "../components/footer/Footer";
+import NavBar from "../components/navbar/NavBar";
 
 export default function Main() {
   return (
     <>
+      <NavBar />
+      <div style={{ height: "80vh" }}>hello</div>
       <Footer />
     </>
   );
