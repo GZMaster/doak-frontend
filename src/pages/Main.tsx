646e419b2c463c8291d41255448cf8594b95ce27
Osianic <56081021+Greg-Soap@users.noreply.github.com>
2023-03-19 06:41:45 +0100

    newsletter completed

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,11 +1,13 @@
 import Footer from "../components/footer/Footer";
 import NavBar from "../components/navbar/NavBar";
+import Newsletter from "../components/news/Newsletter";
 
 export default function Main() {
   return (
     <>
       <NavBar />
       <div style={{ height: "80vh" }}>hello</div>
+      <Newsletter />
       <Footer />
     </>
   );
