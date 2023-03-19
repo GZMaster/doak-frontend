9751c5849bb70e7ed9f5a6080914669b645ef9ff
Osianic <56081021+Greg-Soap@users.noreply.github.com>
2023-03-19 11:49:13 +0100

    hero and sidebar completed

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,11 +1,15 @@
 import Footer from "../components/footer/Footer";
+import Hero from "../components/hero/Hero";
 import NavBar from "../components/navbar/NavBar";
 import Newsletter from "../components/news/Newsletter";
+import Sidebar from "../components/sidebar/Sidebar";
 
 export default function Main() {
   return (
     <>
       <NavBar />
+      <Hero />
+      <Sidebar />
       <div style={{ height: "80vh" }}>hello</div>
       <Newsletter />
       <Footer />
