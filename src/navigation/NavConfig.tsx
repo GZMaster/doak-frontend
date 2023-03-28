f4a7792696cf6e7a27a0de31c8446dac536b17fc
Osianic <56081021+Greg-Soap@users.noreply.github.com>
2023-03-28 22:43:39 +0100

    current code

--- src/navigation/NavConfig.tsx
+++ src/navigation/NavConfig.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import App from "../App";
+import Cart from "../pages/Cart/Cart";
 import Main from "../pages/Main";
 import ProductPage from "../pages/ProductPage/ProductPage";
 
@@ -10,6 +11,7 @@ const NavConfig = () => {
       <Route path="/" element={<App />}>
         <Route path="/" element={<Main />} />
         <Route path="/product" element={<ProductPage />} />
+        <Route path="/cart" element={<Cart />} />
       </Route>
     </Routes>
   );
