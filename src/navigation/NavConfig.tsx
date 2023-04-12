5cd490d01480261c08f59b83513f81670c1c5b9a
GZMaster <33158650+GZMaster@users.noreply.github.com>
2023-04-13 00:22:37 +0100

    working on the authentication page

--- src/navigation/NavConfig.tsx
+++ src/navigation/NavConfig.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import App from "../App";
-import Cart from "../pages/Cart/Cart";
 import Main from "../pages/Main";
+import Cart from "../pages/Cart/Cart";
 import ProductPage from "../pages/ProductPage/ProductPage";
+import SigninPage from "../pages/signin/SigninPage";
 
 const NavConfig = () => {
   return (
@@ -12,6 +13,7 @@ const NavConfig = () => {
         <Route path="/" element={<Main />} />
         <Route path="/product" element={<ProductPage />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/identification" element={<SigninPage />} />
       </Route>
     </Routes>
   );
