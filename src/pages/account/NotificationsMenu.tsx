e1c51e434e9b4ce2d1c6e9c2878b8ad818462d98
GZMaster <33158650+GZMaster@users.noreply.github.com>
2023-04-18 10:37:40 +0100

    notification menu

--- src/pages/account/NotificationsMenu.tsx
+++ src/pages/account/NotificationsMenu.tsx
@@ -1,8 +1,44 @@
 import React from "react";
 import "./AccountPage.scss";
 
+const notifications = [
+  {
+    id: 1,
+    title: "New Message",
+    description: "You have a new message from John Doe",
+  },
+  {
+    id: 2,
+    title: "New Message",
+    description: "You have a new message from John Doe",
+  },
+];
+
 const NotificationsMenu = () => {
-  return <></>;
+  return (
+    <div className="notificationsmenu">
+      <div className="notificationsmenu_header">
+        <h3>All Notifications</h3>
+        <button>Clear All</button>
+      </div>
+
+      <div className="notificationsmenu_body">
+        {notifications.map((notification) => (
+          <div
+            className="notificationsmenu_body_notification"
+            key={notification.id}
+          >
+            <div className="notificationsmenu_body_notification_title">
+              {notification.title}
+            </div>
+            <div className="notificationsmenu_body_notification_description">
+              {notification.description}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default NotificationsMenu;
