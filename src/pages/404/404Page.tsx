a166a0bd522845b31b820ef76cd79f4ec147a25d
Osianic <56081021+Greg-Soap@users.noreply.github.com>
2023-04-20 11:22:19 +0100

    changes

--- src/pages/404/404Page.tsx
+++ src/pages/404/404Page.tsx
@@ -7,19 +7,21 @@ const Page404 = () => {
   return (
     <div className="page404">
       <div className="header">
-        
         <div className="errText">
-            <h1>404</h1>
-            <h2> Oops! Nothing to see here, this page does not exist!</h2>
+          <h1>404</h1>
+          <h2> Oops! Nothing to see here, this page does not exist!</h2>
         </div>
-        
       </div>
       <div className="page404__body">
-        <button className='backbutton' onClick={() => {navigate("/")}}>
+        <button
+          className="backbutton"
+          onClick={() => {
+            navigate("/");
+          }}
+        >
           <img src="arrow-left.svg" alt="icons" />
-          
-              <p>Go back Home</p>
-        
+
+          <p>Go back Home</p>
         </button>
       </div>
     </div>
