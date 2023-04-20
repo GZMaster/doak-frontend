ec1fd99cd4a96fae75f01b57b904ad08eb97b903
Rwinkah <84604851+Rwinkah@users.noreply.github.com>
2023-04-20 07:54:25 +0100

    404page

--- src/pages/404/404Page.tsx
+++ src/pages/404/404Page.tsx
@@ -4,13 +4,20 @@ import "./404Page.scss";
 const Page404 = () => {
   return (
     <div className="page404">
-      <div className="page404__header">
-        <h1>404 Opps! Nothing to see here, this page does not exist!</h1>
+      <div className="header">
+        
+        <div className="errText">
+            <h1>404</h1>
+            <h2> Oops! Nothing to see here, this page does not exist!</h2>
+        </div>
+        
       </div>
       <div className="page404__body">
-        <button>
-          <img src="backicon" alt="icon" />
-          <p>Go back Home</p>
+        <button className='backbutton'>
+          <img src="arrow-left.svg" alt="icons" />
+          
+              <p>Go back Home</p>
+        
         </button>
       </div>
     </div>
