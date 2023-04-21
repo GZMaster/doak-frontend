0d9830272cbb9c7d9f7e9240836df6c07dbaa68f
GZMaster <33158650+GZMaster@users.noreply.github.com>
2023-04-21 09:21:22 +0100

    responsiveness

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -2,12 +2,13 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-scroll";
 import Loader from "react-loader-advanced";
 import Loading from "../../components/Loader/Loading";
+import UseMediaQuery from "../mediaquery/UseMediaQuerry";
 import "./hero.scss";
-import BG from "../../assets/Images/others/hero-bg.svg";
 import shoppingcart from "../../assets/Images/icons/shopping-cart-white.svg";
 
 export default function Hero() {
   const [loading, isLoading] = useState<boolean>(false);
+  const isPageWide = UseMediaQuery("(min-width: 769px)");
 
   const handleLoading = () => {
     isLoading(true);
@@ -18,7 +19,7 @@ export default function Hero() {
 
   useEffect(() => {
     handleLoading();
-  }, [BG, window.onload]);
+  }, [window.onload]);
 
   const spinner = <Loading />;
   return (
@@ -28,20 +29,26 @@ export default function Hero() {
       backgroundStyle={{ backgroundColor: "grey", opacity: 0.9 }}
     >
       <section className="hero">
-        <img src={BG} alt="" />
-        <div className="text-container">
-          <h1>We give you only the best. Top notch quality services.</h1>
-          <p>
-            Integer quam adipiscing vestibulum nunc. Id nunc amet at sed orci
-            diam. Cras venenatis sit faucibus.
-          </p>
-          <Link className="hero__button" smooth={true} spy={true} to="product">
-            <h3>Start Shopping</h3>
-            <span>
-              <img src={shoppingcart} alt="" />
-            </span>
-          </Link>
-        </div>
+        {isPageWide && (
+          <div className="text-container">
+            <h1>We give you only the best. Top notch quality services.</h1>
+            <p>
+              Integer quam adipiscing vestibulum nunc. Id nunc amet at sed orci
+              diam. Cras venenatis sit faucibus.
+            </p>
+            <Link
+              className="hero__button"
+              smooth={true}
+              spy={true}
+              to="product"
+            >
+              <h3>Start Shopping</h3>
+              <span>
+                <img src={shoppingcart} alt="" />
+              </span>
+            </Link>
+          </div>
+        )}
       </section>
     </Loader>
   );
