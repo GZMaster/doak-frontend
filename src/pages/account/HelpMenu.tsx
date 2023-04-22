e17787abc0ee454de76cf93f7864d6f85205245d
GZMaster <33158650+GZMaster@users.noreply.github.com>
2023-04-22 14:17:17 +0100

    mobile account pages

--- src/pages/account/HelpMenu.tsx
+++ src/pages/account/HelpMenu.tsx
@@ -13,19 +13,20 @@ const HelpMenu = () => {
   return (
     <div className="helpmenu">
       <div className="helpmenu_header">
-        <span>
-        Chat Support
-        </span>
+        <span>Chat Support</span>
       </div>
 
       <div className="helpmenu_body">
         <div className="helpmenu_body__chat">{/* MESSAGES GO HERE */}</div>
 
         <div className="helpmenu_body__input">
-          <div className="helpmenu_body__input___field"> 
-          <InputFields type="string" placeholder="What can we help you with" />
+          <div className="helpmenu_body__input___field">
+            <InputFields
+              type="string"
+              placeholder="What can we help you with"
+            />
           </div>
-          
+
           <button onSubmit={() => handleSendMessage(messages)}>
             Send Message
           </button>
