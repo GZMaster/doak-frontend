5e86ee97ae9443e747a597fa7f5b94d55a26604c
GZMaster <33158650+GZMaster@users.noreply.github.com>
2023-04-23 17:33:15 +0100

    removing loading from hero component

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,36 +1,31 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Link } from "react-scroll";
-import Loader from "react-loader-advanced";
-import Loading from "../../components/Loader/Loading";
 import UseMediaQuery from "../mediaquery/UseMediaQuerry";
 import "./hero.scss";
 import shoppingcart from "../../assets/Images/icons/shopping-cart-white.svg";
 import heroBg from "../../assets/Images/others/hero-mobile.png";
 
 export default function Hero() {
-  const [loading, isLoading] = useState<boolean>(false);
   const isPageWide = UseMediaQuery("(min-width: 769px)");
 
-  const handleLoading = () => {
-    isLoading(true);
-    setTimeout(() => {
-      isLoading(false);
-    }, 2000);
-  };
-
-  useEffect(() => {
-    handleLoading();
-  }, [window.onload]);
-
-  const spinner = <Loading />;
   return (
-    <Loader
-      show={loading}
-      message={spinner}
-      backgroundStyle={{ backgroundColor: "grey", opacity: 0.9 }}
-    >
-      <section className="hero">
-        {isPageWide ? (
+    <section className="hero">
+      {isPageWide ? (
+        <div className="text-container">
+          <h1>We give you only the best. Top notch quality services.</h1>
+          <p>
+            Integer quam adipiscing vestibulum nunc. Id nunc amet at sed orci
+            diam. Cras venenatis sit faucibus.
+          </p>
+          <Link className="hero__button" smooth={true} spy={true} to="product">
+            <h3>Start Shopping</h3>
+            <span>
+              <img src={shoppingcart} alt="" />
+            </span>
+          </Link>
+        </div>
+      ) : (
+        <div style={{ display: "flex", flexDirection: "column" }}>
           <div className="text-container">
             <h1>We give you only the best. Top notch quality services.</h1>
             <p>
@@ -49,30 +44,9 @@ export default function Hero() {
               </span>
             </Link>
           </div>
-        ) : (
-          <div style={{ display: "flex", flexDirection: "column" }}>
-            <div className="text-container">
-              <h1>We give you only the best. Top notch quality services.</h1>
-              <p>
-                Integer quam adipiscing vestibulum nunc. Id nunc amet at sed
-                orci diam. Cras venenatis sit faucibus.
-              </p>
-              <Link
-                className="hero__button"
-                smooth={true}
-                spy={true}
-                to="product"
-              >
-                <h3>Start Shopping</h3>
-                <span>
-                  <img src={shoppingcart} alt="" />
-                </span>
-              </Link>
-            </div>
-            <img src={heroBg} alt="" />
-          </div>
-        )}
-      </section>
-    </Loader>
+          <img src={heroBg} alt="" />
+        </div>
+      )}
+    </section>
   );
 }
