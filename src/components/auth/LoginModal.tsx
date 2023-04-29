5f8ff7c47bf675a0a3567ef74f04f629b1cd2d45
GZMaster <33158650+GZMaster@users.noreply.github.com>
2023-04-29 09:59:12 +0100

    changes

--- src/components/auth/LoginModal.tsx
+++ src/components/auth/LoginModal.tsx
@@ -1,14 +1,24 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { AuthContext } from "../../services/AuthContext";
 import { InputFields } from "../../lib/Main";
 import ForgotPassModal from "../forgotpass/ForgotPassModal";
 import "./AuthModal.scss";
 
-const LoginPage = () => {
+interface LoginPageProps {
+  onClose: () => void;
+}
+
+const LoginPage: React.FC<LoginPageProps> = ({ onClose }) => {
   const authContext = useContext(AuthContext);
   const [forgotPass, setForgotPass] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    if (authContext.isLoggedIn) {
+      onClose();
+    }
+  }, [authContext.isLoggedIn, onClose]);
 
   const handleModal = () => {
     setForgotPass(false);
@@ -16,7 +26,8 @@ const LoginPage = () => {
 
   const handleLogin = (email: string, password: string) => {
     authContext.login(email, password).then((res) => {
-      console.log(res);
+      authContext.setIsLoggedIn(res);
+
       // THIS WILL REDIRECT TO DASHBOARD IF ADMIN IS LOGGED IN
       // if (res) {
       //   navigate("/dashboard");
