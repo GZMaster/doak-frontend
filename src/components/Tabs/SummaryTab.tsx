cea1e50065d0094545a5e73c492cb0d49386f376
GZMaster <33158650+GZMaster@users.noreply.github.com>
2023-05-02 03:58:28 +0100

    checkout workflow

--- src/components/Tabs/SummaryTab.tsx
+++ src/components/Tabs/SummaryTab.tsx
@@ -1,14 +1,107 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { FormatNaira } from "../../utils/FormatCurrency";
 import product from "../../assets/Images/others/itemDrink.png";
 import UseMediaQuery from "../mediaquery/UseMediaQuerry";
 import "./Tab.scss";
 
 interface Props {
   handleTabClick: (key: number) => void;
+  cartItems: [
+    {
+      name: string;
+      price: number;
+      product: string;
+      quantity: number;
+    }
+  ];
 }
 
-const SummaryTab: React.FC<Props> = ({ handleTabClick }) => {
+interface Address {
+  userId?: string;
+  name: string;
+  address: string;
+  city: string;
+  phoneNumber: string;
+  state: string;
+  country: string;
+  zipCode?: string;
+  _id: string;
+}
+
+const SummaryTab: React.FC<Props> = ({ handleTabClick, cartItems }) => {
   const isPageWide = UseMediaQuery("(min-width: 769px)");
+  const navigate = useNavigate();
+  const [address, setAddress] = useState<Address>();
+
+  useEffect(() => {
+    getDefaultAddress();
+  }, []);
+
+  const getTotal = () => {
+    let total = 0;
+    if (cartItems) {
+      Object.values(cartItems).forEach((item) => {
+        total += item.price * item.quantity;
+      });
+    }
+
+    return total;
+  };
+
+  const createOrder = async () => {
+    // Get jwt Bear token from local storage
+    const token = localStorage.getItem("jwt");
+    const total = getTotal();
+
+    if (!address) {
+      return;
+    }
+
+    const res = await fetch(
+      // `https://doakbackend.cyclic.app/api/v1/orders`,
+      `http://localhost:3000/api/v1/orders`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          address: address._id,
+          item: cartItems,
+          subtotal: total,
+        }),
+      }
+    );
+
+    const response = await res.json();
+
+    if (response.status === "success") {
+      handleTabClick(2);
+    }
+  };
+
+  const getDefaultAddress = async () => {
+    // Get jwt Bear token from local storage
+    const token = localStorage.getItem("jwt");
+
+    const res = await fetch(
+      // `https://doakbackend.cyclic.app/api/v1/addresses/default`,
+      `http://localhost:3000/api/v1/addresses/default`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    const response = await res.json();
+
+    setAddress(response.data.addressData);
+  };
 
   return (
     <section className="summary_tab">
@@ -17,54 +110,28 @@ const SummaryTab: React.FC<Props> = ({ handleTabClick }) => {
         <div className="wrapper">
           <div className="summary_tab_header">
             <p>Cart Items</p>
-            <p className="summary_tab_edit">Edit</p>
+            <p className="summary_tab_edit" onClick={() => navigate("/cart")}>
+              Edit
+            </p>
           </div>
           <div className="summary_tab_body">
-            <div className="item">
-              <div className="product__cart">
-                <img className="product__image" src={product} alt="" />
-                <div className="product__details">
-                  <p className="product__name">
-                    Hennessy VS Cognac ORIGINAL 70cl X6
-                  </p>
-                  {isPageWide && <p>70cl</p>}
-                  {!isPageWide && <p className="quantity">QTY:x10</p>}
-                </div>
-              </div>
-              {isPageWide && <p className="quantity">x10</p>}
+            {cartItems &&
+              cartItems.map((item) => (
+                <div className="item" key={item.product}>
+                  <div className="product__cart">
+                    <img className="product__image" src={product} alt="" />
+                    <div className="product__details">
+                      <p className="product__name">{item.name}</p>
+                      {!isPageWide && (
+                        <p className="quantity">QTY:x{item.quantity}</p>
+                      )}
+                    </div>
+                  </div>
+                  {isPageWide && <p className="quantity">x{item.quantity}</p>}
 
-              <div className="price">N23,000</div>
-            </div>
-            <div className="item">
-              <div className="product__cart">
-                <img className="product__image" src={product} alt="" />
-                <div className="product__details">
-                  <p className="product__name">
-                    Hennessy VS Cognac ORIGINAL 70cl X6
-                  </p>
-                  {isPageWide && <p>70cl</p>}
-                  {!isPageWide && <p className="quantity">QTY:x10</p>}
+                  <div className="price">{FormatNaira(item.price)}</div>
                 </div>
-              </div>
-              {isPageWide && <p className="quantity">x10</p>}
-
-              <div className="price">N23,000</div>
-            </div>
-            <div className="item">
-              <div className="product__cart">
-                <img className="product__image" src={product} alt="" />
-                <div className="product__details">
-                  <p className="product__name">
-                    Hennessy VS Cognac ORIGINAL 70cl X6
-                  </p>
-                  {isPageWide && <p>70cl</p>}
-                  {!isPageWide && <p className="quantity">QTY:x10</p>}
-                </div>
-              </div>
-              {isPageWide && <p className="quantity">x10</p>}
-
-              <div className="price">N23,000</div>
-            </div>
+              ))}
           </div>
         </div>
         <div className="wrapper">
@@ -72,11 +139,15 @@ const SummaryTab: React.FC<Props> = ({ handleTabClick }) => {
             <p>Address</p>
             <p className="edit">Edit</p>
           </div>
-          <div className="summary_tab_body">
-            <h2>Omonaluse Ohkuehne</h2>
-            <p>No 14, 19th street BDPA, Ugbowo, Benin City, Oyo State</p>
-            <p>+2348180281937</p>
-          </div>
+          {address && (
+            <div className="summary_tab_body" key={address.userId}>
+              <h2>{address.name}</h2>
+              <p>
+                {address.address}, {address.city} City, {address.state} State
+              </p>
+              <p>{address.phoneNumber}</p>
+            </div>
+          )}
         </div>
         <div className="wrapper">
           <div className="summary_tab_header">
@@ -92,7 +163,7 @@ const SummaryTab: React.FC<Props> = ({ handleTabClick }) => {
         <div
           style={{ textAlign: "center" }}
           className="auth_continue_btn"
-          onClick={() => handleTabClick(2)}
+          onClick={() => createOrder()}
         >
           Proceed to Payment
         </div>
