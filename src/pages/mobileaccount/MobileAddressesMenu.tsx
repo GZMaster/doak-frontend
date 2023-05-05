51126852af22b0370e6a07be2ea995acd9067a11
GZMaster <33158650+GZMaster@users.noreply.github.com>
2023-05-05 01:32:50 +0100

    addresses

--- src/pages/mobileaccount/MobileAddressesMenu.tsx
+++ src/pages/mobileaccount/MobileAddressesMenu.tsx
@@ -1,22 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AddAddressModal from "../../components/address/AddAddressModal";
 import "./MobileAccountPage.scss";
 import backbtn from "../../assets/Images/icons/backbtn.svg";
 
-const addresses = [
-  {
-    id: 1,
-    title: "Home",
-    address: "1234 Main St, New York, NY 10001",
-    phonenumber: "123-456-7890",
-  },
-  {
-    id: 2,
-    title: "Work",
-    address: "1234 Main St, New York, NY 10001",
-    phonenumber: "123-456-7890",
-  },
-];
+interface Address {
+  userId?: string;
+  name: string;
+  address: string;
+  city: string;
+  phoneNumber: string;
+  state: string;
+  country: string;
+  zipCode?: string;
+  _id: string;
+}
 
 interface MobileAddressesMenuProps {
   handleBack: () => void;
@@ -26,6 +23,33 @@ const MobileAddressesMenu: React.FC<MobileAddressesMenuProps> = ({
   handleBack,
 }) => {
   const [isOpen, setIsOpen] = React.useState(false);
+  const [addresses, setAddresses] = useState<Address[]>([]);
+
+  useEffect(() => {
+    getAddresses();
+  }, []);
+
+  const getAddresses = async () => {
+    const response = await fetch(
+      // "https://doakbackend.cyclic.app/api/v1/addresses",
+      "http://localhost:3000/api/v1/addresses",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+        },
+      }
+    );
+
+    const data = await response.json();
+
+    const addresses = data.data.addresses;
+
+    if (data.status === "success") {
+      setAddresses(addresses);
+    }
+  };
 
   const handleAddressSave = () => {
     setIsOpen(false);
@@ -49,22 +73,23 @@ const MobileAddressesMenu: React.FC<MobileAddressesMenuProps> = ({
       </div>
 
       <div className="mobileaddressesmenu_body">
-        {addresses.map((address) => (
-          <div className="mobileaddressesmenu_address" key={address.id}>
-            <div className="mobileaddressesmenu_address_container">
-              <div className="mobileaddressesmenu_detials">
-                <h4>{address.title}</h4>
-                <p>{address.address}</p>
-                <p>{address.phonenumber}</p>
-              </div>
+        {addresses &&
+          addresses.map((address) => (
+            <div className="mobileaddressesmenu_address" key={address._id}>
+              <div className="mobileaddressesmenu_address_container">
+                <div className="mobileaddressesmenu_detials">
+                  <h4>{address.name}</h4>
+                  <p>{address.address}</p>
+                  <p>{address.phoneNumber}</p>
+                </div>
 
-              <div className="mobileaddressesmenu_address_actions">
-                <button className="edit">Edit</button>
-                <button className="delete">Delete</button>
+                <div className="mobileaddressesmenu_address_actions">
+                  {/* <button className="edit">Edit</button> */}
+                  <button className="delete">Delete</button>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
 
         <div className="mobileaddressesmenu_addaddress">
           <button onClick={() => setIsOpen(true)}>+ Add New Address</button>
