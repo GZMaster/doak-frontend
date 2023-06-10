2e8e033dd2ae5981c57062c0b38b675ec674f718
Osianic <56081021+Greg-Soap@users.noreply.github.com>
2023-06-10 11:15:43 +0100

    changes

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -16,7 +16,6 @@ export default function Cart() {
   const isPageWide = UseMediaQuery("(min-width: 769px)");
   const [cartLength, setCartLength] = useState(0);
   const [totalPrice, setTotalPrice] = useState(0);
-
   useEffect(() => {
     setIsLoading(true);
 
@@ -90,7 +89,6 @@ export default function Cart() {
                                   -
                                 </button>
                                 <input
-                                  type="number"
                                   name="quantity"
                                   value={quantity}
                                   maxLength={3}
@@ -192,7 +190,6 @@ export default function Cart() {
                                   -
                                 </button>
                                 <input
-                                  type="text"
                                   name="quantity"
                                   value={quantity}
                                   maxLength={3}
